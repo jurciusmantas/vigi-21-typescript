1e76cfa8443a86812eb7a5ac1106ec3cb5f3b128
Mantas Jurčius <jurciusmantas@gmail.com>
2022-09-28 19:31:10 +0300

    10 paskaita done

--- src/9_paskaita/9_paskaita.ts
+++ src/9_paskaita/9_paskaita.ts
@@ -1,56 +1,51 @@
-class Animal {
-  protected name: string;
-  protected sex: string;
-  protected legs: number;
+// // SOLID
+// // D - DRY (Dont Repeat Yourself)
+// class Animal {
+//   protected name: string;
+//   protected sex: string;
+//   protected legs: number;
 
-  constructor(name: string, sex: string, legs: number) {
-    this.name = name;
-    this.sex = sex;
-    this.legs = legs;
-  }
+//   constructor(name: string, sex: string, legs: number) {
+//     this.name = name;
+//     this.sex = sex;
+//     this.legs = legs;
+//   }
 
-  eat() {
-    console.log(`Animal ${this.name} is eating...`);
-  }
-}
+//   eat() {
+//     console.log(`Animal ${this.name} is eating...`);
+//   }
+// }
 
-class Dog extends Animal {
-  protected breed: string;
+// class Dog extends Animal {
+//   makeNoise() {
+//     console.log(`Bark bark, my name is ${this.name}`);
+//   }
+// }
 
-  constructor(name: string, sex: string, legs: number, breed: string) {
-    super(name, sex, legs);
-    this.breed = breed;
-  }
+// class Cat extends Animal {
+//   protected breed: string;
+//   private dislikes: string;
 
-  makeNoise() {
-    console.log(`Bark bark, my name is ${this.name}`);
-  }
-}
+//   constructor(name: string, sex: string, legs: number, breed: string) {
+//     super(name, sex, legs);
+//     this.breed = breed;
+//     this.dislikes = 'Dog';
+//   }
 
-class Cat extends Animal {
-  protected breed: string;
-  private dislikes: string;
+//   makeNoise() {
+//     console.log(`Miau miau, I don't like ${this.dislikes}`);
+//   }
 
-  constructor(name: string, sex: string, legs: number, breed: string) {
-    super(name, sex, legs);
-    this.breed = breed;
-    this.dislikes = 'Dog';
-  }
+//   override eat() {
+//     console.log('Cat refuses to eat now');
+//   }
+// }
 
-  makeNoise() {
-    console.log(`Miau miau, I don't like ${this.dislikes}`);
-  }
+// const animal1: Animal = new Animal('Rika', 'female', 4);
+// animal1.eat();
 
-  override eat() {
-    console.log('Cat refuses to eat now');
-  }
-}
+// const dog1: Dog = new Dog('Rikis', 'male', 4);
+// dog1.eat();
 
-const animal1: Animal = new Animal('Rika', 'female', 4);
-animal1.eat();
-
-const dog1: Dog = new Dog('Rikis', 'male', 4, 'Pitbulis');
-dog1.eat();
-
-const cat1: Cat = new Cat('Pukis', 'male', 4, 'Lietuvinis');
-cat1.eat();
+// const cat1: Cat = new Cat('Pukis', 'male', 4, 'Lietuvinis');
+// cat1.eat();
--- src/main/main.ts
+++ src/main/main.ts
@@ -1,1 +1,120 @@
-console.log('labas');
+abstract class Animal {
+  protected energy: number;
+  protected weight: number;
+
+  constructor(energy: number, weight: number) {
+    this.energy = energy;
+    this.weight = weight;
+  }
+
+  /* Template design pattern start */
+  wakeUp() {
+    console.log('Animal woke up');
+  }
+
+  abstract eat(food: number): void;
+  abstract move(): void;
+
+  goSleep() {
+    console.log('Animal going to sleep');
+  }
+
+  dailyLife() {
+    this.wakeUp();
+    this.eat(20);
+    this.move();
+    this.eat(20);
+    this.goSleep();
+  }
+
+  /* Template desing pattern end */
+}
+
+// const animal1: Animal = new Animal();
+
+class Dog extends Animal {
+  public name: string;
+
+  constructor(energy: number, weight: number, name: string) {
+    super(energy, weight);
+    this.name = name;
+  }
+
+  eat(food: number) {
+    this.energy += food * 0.01;
+    this.weight += food * 0.1; 
+  }
+
+  move() {
+    console.log(`Dog ${this.name} is moving...`);
+  }
+}
+
+class GuardDog extends Dog {
+
+}
+
+class Cat extends Animal {
+  eat(food: number) {
+    this.energy += food * 0.005;
+    this.weight += food * 0.07; 
+  }
+
+  move() {
+    console.log(`Cat is sitting...`);
+  }
+
+  override goSleep(): void {
+    console.log('No sleep, cat is going to hunt');
+  }
+
+  showAffection() {
+    console.log('MurMur');
+  }
+}
+
+const cat1: Animal = new Cat(100, 7);
+// cat1.showAffection();
+cat1.dailyLife();
+
+const feedAnimal = (animal: Animal, food: number): void => {
+  animal.eat(food);
+};
+const dog1: Dog = new Dog(200, 15, 'Bruno');
+const guardDog1: GuardDog = new GuardDog(300, 20, 'Sargis');
+
+feedAnimal(cat1, 10);
+feedAnimal(dog1, 50);
+feedAnimal(guardDog1, 100);
+
+type AnimalShelter = {
+  animals: Animal[]
+};
+
+const shelter1: AnimalShelter = {
+  animals: [
+    dog1,
+    guardDog1,
+    cat1,
+  ],
+};
+
+/* Polimorfizma galime naudoti netik su abstrakcija */
+class Mover {
+  move() {
+    console.log('I am moving...');
+  }
+}
+
+class Human extends Mover {
+
+}
+
+const human: Mover = new Human();
+const moveAllMover = (movers: Mover[]): void => {
+  movers.forEach((mover) => mover.move());
+};
+moveAllMover([
+  new Human(),
+  new Mover(),
+]);
